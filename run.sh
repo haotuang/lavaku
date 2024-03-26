b2ae7c510bcea44a4aa052d631c6c5b55ae9cceb
Hao Tuang <165036005+haotuang@users.noreply.github.com>
2024-03-26 20:03:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-da63a61a-f960-4c20-86cf-87e2c62c70a5/)
 
 
 echo "$curl_output" >> Update.tsx
